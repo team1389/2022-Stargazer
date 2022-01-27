25329a70e424f70eceef4a1d97984d9bcc4a1926
NoorMehn <98059574+NoorMehn@users.noreply.github.com>
2022-01-27 16:27:50 -0500

    ball autos

---
+++ src/main/java/frc/autos/OneBallAuto.java
@@ -0,0 +1,16 @@
+package frc.autos;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.commands.FollowPath;
+import frc.commands.ShootWithSensors;
+import frc.robot.Robot;
+
+public class OneBallAuto extends SequentialCommandGroup{
+    public OneBallAuto() {
+        addRequirements(Robot.drivetrain, Robot.shooter);
+        addCommands(
+            new ShootWithSensors(),
+            new FollowPath("OneBallAuto")
+        );
+    }
+}
