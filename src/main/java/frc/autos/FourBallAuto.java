06175f5a1741a84ec169f9a569df08980f0780ae
NoorMehn <98059574+NoorMehn@users.noreply.github.com>
2022-02-14 16:26:48 -0500

    Added intake to autos

--- src/main/java/frc/autos/FourBallAuto.java
+++ src/main/java/frc/autos/FourBallAuto.java
@@ -1,15 +1,22 @@
 package frc.autos;
 
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.commands.FollowPath;
 import frc.commands.ShootWithSensors;
+import frc.robot.Robot;
 
 public class FourBallAuto extends SequentialCommandGroup{
     public FourBallAuto() {
+        addRequirements(Robot.drivetrain, Robot.shooter,Robot.intake);
         addCommands(
+            new InstantCommand(() -> Robot.intake.setIntakePercent(0.85)),
             new FollowPath("Four Ball Auto"),
+            new InstantCommand(() -> Robot.intake.setIntakePercent(0.0)),
             new ShootWithSensors(),
+            new InstantCommand(() -> Robot.intake.setIntakePercent(0.85)),
             new FollowPath("Four Ball Auto Pt.Two"),
+            new InstantCommand(() -> Robot.intake.setIntakePercent(0.0)),
             new ShootWithSensors()
         );
 }
--- src/main/java/frc/autos/OneBallAuto.java
+++ src/main/java/frc/autos/OneBallAuto.java
@@ -1,5 +1,6 @@
 package frc.autos;
 
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.commands.FollowPath;
 import frc.commands.ShootWithSensors;
@@ -7,10 +8,12 @@ import frc.robot.Robot;
 
 public class OneBallAuto extends SequentialCommandGroup{
     public OneBallAuto() {
-        addRequirements(Robot.drivetrain, Robot.shooter);
+        addRequirements(Robot.drivetrain, Robot.shooter,Robot.intake);
         addCommands(
             new ShootWithSensors(),
-            new FollowPath("OneBallAuto")
+            new InstantCommand(() -> Robot.intake.setIntakePercent(0.85)),
+            new FollowPath("OneBallAuto"),
+            new InstantCommand(() -> Robot.intake.setIntakePercent(0.0))
         );
     }
 }
