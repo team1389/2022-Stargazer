4a650f750d9b1a824a962858cd7b9bf19add543e
Maxohm491 <maxdemohm@gmail.com>
2022-02-28 15:36:09 -0500

    deleted extra stuff

--- src/main/java/frc/autos/OneBallAuto.java
+++ src/main/java/frc/autos/OneBallAuto.java
@@ -3,17 +3,13 @@ package frc.autos;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.commands.FollowPath;
-import frc.commands.ShootWithSensors;
 import frc.robot.Robot;
 
 public class OneBallAuto extends SequentialCommandGroup{
     public OneBallAuto() {
-        addRequirements(Robot.drivetrain, Robot.shooter,Robot.intake);
+        addRequirements(Robot.drivetrain);
         addCommands(
-            new ShootWithSensors(),
-            new InstantCommand(() -> Robot.intake.setIntakePercent(0.85)),
-            new FollowPath("OneBallAuto"),
-            new InstantCommand(() -> Robot.intake.setIntakePercent(0.0))
+            new FollowPath("OneBallAuto")
         );
     }
 }
