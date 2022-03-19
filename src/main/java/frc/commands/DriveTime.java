3eed8283ac4c0b5c1512e83d1a16935039fe1935
coolcreateK <kilian.taillandier2@gmail.com>
2022-03-19 06:34:04 -0400

    Fixed ash's god awful mistake >:(

--- src/main/java/frc/commands/DriveTime.java
+++ src/main/java/frc/commands/DriveTime.java
@@ -4,6 +4,7 @@
 
 package frc.commands;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -12,9 +13,10 @@ import frc.robot.Robot;
 public class DriveTime extends CommandBase {
   /** Creates a new ManualAuto. */
   Timer timer;
-  double x, y, angle, timeout;
+  double x, y, rotation, timeout, initialAngle;
+  PIDController pid;
 
-  public DriveTime(double timeout, double x, double y, double angle) {
+  public DriveTime(double timeout, double x, double y, double rotation) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
 
@@ -22,7 +24,9 @@ public class DriveTime extends CommandBase {
     this.timeout = timeout;
     this.x = x;
     this.y = y;
-    this.angle = angle;
+    this.rotation = rotation;
+
+    pid = new PIDController(0.01, 0, 0);
 
     addRequirements(Robot.drivetrain);
   }
@@ -31,11 +35,17 @@ public class DriveTime extends CommandBase {
   public void initialize() {
     timer.reset();
     timer.start();
+    
+    initialAngle = Robot.drivetrain.getAngle();
+    pid.setSetpoint(initialAngle);
   }
 
   @Override
   public void execute() {
-    Robot.drivetrain.drive(x, y, angle, 1);
+    double rotationCorrection = pid.calculate(Robot.drivetrain.getAngle());
+    rotationCorrection = Math.max(-0.3, Math.min(0.3, rotationCorrection));
+
+    Robot.drivetrain.drive(x, y, rotation + rotationCorrection, 1);
   }
 
   @Override
